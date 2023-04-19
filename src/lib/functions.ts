37620b72fb82d247d8621b9cd669cdf7953614c7
Stephen <stephengunn@gmail.com>
2023-04-19 14:06:06 -0500

    working with characters

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,1 +1,4 @@
 export const get_random = (array: any) => array[Math.floor(Math.random()*array.length)]
+
+// replace spaces with underscores for happy images
+export const image_url = (string: string) => string.replace(/ /g, "_")
