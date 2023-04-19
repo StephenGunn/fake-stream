2a00b6be325e8b1d670f91faa2b15eecb6fe4f9f
Stephen <stephengunn@gmail.com>
2023-04-19 14:06:06 -0500

    working with characters

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,70 +1,62 @@
 <script lang="ts">
-    import { emojis }     from "$lib/emoji"
-    import { quotes }     from "$lib/quotes"
-    import {
-        get_random,
-        image_url
-    }                     from "$lib/functions"
-    import { onMount }    from "svelte"
-    import { fly }        from "svelte/transition"
+    import { emojis } from "$lib/emoji";
+    import { quotes } from "$lib/quotes";
+    import { get_random, image_url } from "$lib/functions";
+    import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
 
     // vars
-    let chat_box: HTMLElement
+    let chat_box: HTMLElement;
     let chat: {
-        Emoji: string
-        Author: string
-        Quote: string
+        Emoji: string;
+        Author: string;
+        Quote: string;
     }[] = [
         {
             Emoji: "😎",
-            Author: 'Mario',
-            Quote: 'Hello streamer! Welcome to your live chat room!'
-        }
-    ]
+            Author: "Mario",
+            Quote: "Hello streamer! Welcome to your live chat room!",
+        },
+    ];
 
     // funcs
     const add_message = () => {
-        const random_message = get_random(quotes)
+        const random_message = get_random(quotes);
         chat.push({
             Emoji: get_random(emojis),
             Author: random_message.Author,
-            Quote: random_message.Quote
-        })
+            Quote: random_message.Quote,
+        });
 
-        chat = chat
-        chat_box.scrollTop = chat_box.scrollHeight
-    }
+        chat = chat;
+        chat_box.scrollTop = chat_box.scrollHeight;
+    };
 
-    onMount(()=> {
-        setInterval(async ()=> {
-            const random_chance: boolean = Math.random() > 0.8
-            if(random_chance) add_message()
-        }, 10000)
-    })
+    onMount(() => {
+        setInterval(async () => {
+            const random_chance: boolean = Math.random() > 0.8;
+            if (random_chance) add_message();
+        }, 10000);
+    });
 </script>
 
-
 <div class="chat">
-    <div class="title">
-        LIVE CHAT
-    </div>
+    <div class="title">LIVE CHAT</div>
     <div class="room" bind:this={chat_box}>
         {#each chat as message}
-            <div class="message" in:fly={{duration: 500, x: 50}}>
+            <div class="message" in:fly={{ duration: 500, x: 50 }}>
                 <div class="face">
                     <img
                         src="/{image_url(message.Author)}.png"
                         alt="{message.Author} avatar"
-                        width="45"
-                        height="45"
-                    >
+                    />
                 </div>
                 <div class="quote">
                     <div class="author">
                         {message.Author}
                     </div>
                     <div class="text">
-                        {message.Quote} 
+                        {message.Quote}
                         {#if Math.random() > 0.5}
                             {message.Emoji}
                         {/if}
@@ -74,7 +66,6 @@
         {/each}
     </div>
 </div>
-
 
 <style lang="sass">
 .chat
@@ -106,7 +97,9 @@
         display: flex
         gap: 1rem
         max-width: 600px
-
+        .face img
+            width: 2.5rem
+            height: 2.5rem
             
         .quote
             .author
